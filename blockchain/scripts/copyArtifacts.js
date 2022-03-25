f52df72c673f642f9ece18081600c6d729397e13
heycryptobob <heycryptobob@gmail.com>
2022-03-25 18:24:15 +0100

    example contrac, deployment, and artifact copy.

---
+++ blockchain/scripts/copyArtifacts.js
@@ -0,0 +1,14 @@
+const copy = require("copy")
+const util = require("util")
+
+const asyncCopy = util.promisify(copy)
+
+const src = "artifacts/contracts/**/!(*.dbg.json|!(*.json))"
+const dest = "../react-app/src/artifacts/contracts/"
+
+const copyArtifacts = async () => {
+  const files = await asyncCopy(src, dest)
+  console.log(`Copied ${files.length} files.`)
+}
+
+module.exports = copyArtifacts
